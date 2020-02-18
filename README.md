7bc14c506c2385460679a8e6915a310dde1eaf3e
cdlaimin <452959229@qq.com>
2020-02-18 10:11:04 +0800

    [2020-02-18 10:11:04]auto update

---
+++ README.md
@@ -0,0 +1,3 @@
+最后一次更新时间 2020-02-18 10:11:04
+https://raw.githubusercontent.com/cdlaimin/genpac/dev/gfwlist.pac
+
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
+genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080" >> pac.txt
 echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/genpac/dev/gfwlist.pac
 " > /root/genpac/README.md
 git add .
