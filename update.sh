5ad7ec7c79f054036921745cb697e66bfc6b3dff
cdlaimin <452959229@qq.com>
2020-02-18 10:03:33 +0800

    [2020-02-18 10:03:33]auto update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
-echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/gfwlist2pac/master/gfwlist.pac
+echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/genpac/dev/gfwlist.pac
 " > /root/genpac/README.md
 git add .
 git commit -m "[$(LANG=C date +"%Y-%m-%d %H:%M:%S")]auto update"
