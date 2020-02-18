1143fceb85f7cd136d060ffa26c58623dbfe9748
cdlaimin <452959229@qq.com>
2020-02-18 10:12:16 +0800

    [2020-02-18 10:12:16]auto update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 genpac --format=pac --pac-proxy="SOCKS5 127.0.0.1:1080" >> pac.txt
-echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/genpac/dev/gfwlist.pac
+echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/genpac/dev/pac.txt
 " > /root/genpac/README.md
 git add .
 git commit -m "[$(LANG=C date +"%Y-%m-%d %H:%M:%S")]auto update"
